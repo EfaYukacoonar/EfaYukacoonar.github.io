a09368c9841f18d2487206350a9ed72b19b1cb52
EfaYukacoonar <efayukacoonar@gmail.com>
2026-02-02 00:43:54 +0900

    a

--- games/fps/environment.js
+++ games/fps/environment.js
@@ -1,2 +1,39 @@
-const API_KEY = 6ad7c3b262384cf1ac9133000260102
+const API_KEY = '6ad7c3b262384cf1ac9133000260102';
 // till 2026-02-15
+export async function getEnvironmentData(lat, lon) {
+    const url = 'https://api.weatherapi.com/vl/forecast.json?key=${API_KEY}&q=${lat},${lon}&days=1&aqi=no&alerts=no';
+    try {
+        const respons = await fetch(url);
+        if (!respons.ok) throw new console.error('Failed the API requeat');
+        const data = await respons.json();
+        return {
+            temp: data.current.temp_c,
+            pressure: data.current.pressure_mb,
+            humidity: data.current.humidity,
+            wind: {
+                speed: data.current.wind_kph / 3.6,
+                deg: data.current.wind_degree
+            },
+            sun: {
+                sunrise: data.forecast.forecastday[0].astro.sunrise,
+                sunset: data.forecast.forecastday[0].astro.sunset
+            },
+            visibility: data.current.vis_km        
+        };
+    }
+    catch (e) {console.error("Failed getting the weather data:", e)
+        return {
+            temp: 15,
+            pressure: 1013.25,
+            humidity: 0,
+            wind: {
+                speed: 0,
+                deg: 0
+            },
+            sun: {
+                unrise: "06:00 AM",
+                sunset: "06:00 PM"
+            }
+        };
+    }
+}
