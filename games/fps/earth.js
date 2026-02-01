f671acde1b9814a7cabf3d6c704f853d7dbd8f24
EfaYukacoonar <efayukacoonar@gmail.com>
2026-02-01 19:38:45 +0900

    a

---
+++ games/fps/earth.js
@@ -0,0 +1,34 @@
+const WGS84 = {
+    a: 6378137.0,
+    f: 1 / 298.257223563,
+    get b () {
+        return this.a * (1 - this.f);
+    },
+    get e2() {
+        return (2 * this.f) - (this.f * this.f);
+    },
+    omega: 7.292115e-5,
+    GM: 3.986004418e14
+}
+
+export function geoToECEF(lat, lon, alt) {
+    const radLat = lat * Math.PI / 180;
+    const radLat = lon * Math.PI / 180;
+    const N = WGS84.a / Math.sqrt(1 - WGS84.e2 * Math.sin(radLat)**2);
+    const x = (N + alt) * Math.cos(radLat) * Math.cos(radLat);
+    const y = (N + alt) * Math.cos(radLat) * Math.sin(radLat);
+    const z = (N * (1 - WGS84.e2) + alt) * Math.sin(radLat);
+
+    return {x, y, z};
+}
+
+export function getNormalGravity(lat) {
+    const radLat = lat * Math.PI / 180;
+    const sin2 = Math.sin(radLat)**2;
+    const ge = 9.7803253359;
+    const gp = 9.8321849378;
+    const k = (WGS84.b * gp) / (WGS84.a * ge) - 1;
+    const g0 = ge * (1 + k * sin2) / Math.sqrt(1 - WGS84.e2 * sin2);
+
+    return g0;
+}
