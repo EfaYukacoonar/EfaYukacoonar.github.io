d9f08f065bc26b7147fa822d15722ed811742429
EfaYukacoonar <efayukacoonar@gmail.com>
2026-05-05 17:21:30 +0900

    miss me

--- games/fps/main.js
+++ games/fps/main.js
@@ -1,1 +1,24 @@
-//時刻、日の出、日の入り、太陽の角度と太陽光、光
+import * as THREE from 'three';
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
+import { Kinematics } from './kinematics.js';
+import { getEnvironmentData } from './environment.js';
+import { getNormalGravity } from './earth';
+
+const scene = new THREE.scene();
+scene.background = new THREE.Color(0x87ceeb);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 20000);
+camera.position.set(0, 1.6, 0);
+const renderer = new THREE.WebGLRenderer({ antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+const ambientLight = new THREE.ambientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const size = 10000;
+const divisions = 1000;
+const gridHelper = new THREE.gridHelper(size, divisions, 0x0000ff, 0x808080);
+scene.add(gridHelper);
+
+const floorGeometry = new THREE.PlaneGeometry(size, size);
