9b7cb045c2717c2327630b053b19b6056731cc8a
EfaYukacoonar <efayukacoonar@gmail.com>
2026-05-06 16:21:54 +0900

    shlt

--- games/fps/main.js
+++ games/fps/main.js
@@ -22,3 +22,52 @@ const gridHelper = new THREE.gridHelper(size, divisions, 0x0000ff, 0x808080);
 scene.add(gridHelper);
 
 const floorGeometry = new THREE.PlaneGeometry(size, size);
+const floorMaterial = new THREE.MeshStandardMaterial({ color: 0x228b22, side: THREE.DoubleSide }),
+const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.rotation.x = -Math.PI / 2;
+scene.add(floor);
+
+const lat = 35.0116;
+const lon = 135.7681;
+let worldContext = {
+    lat: lat,
+    temp: 15,
+    pressure: 1013.25,
+    humidity: 50,
+    wind: { speed: 0, direction: 0 }
+};
+
+const gravity = getNormalGravity(lat);
+getEnvironmentData(lat, lon).then(data => {
+    worldContext = {
+        ...worldContext, ...data, lat: lat
+    };
+    console.log("Environment loaded:", worldContext);
+});
+
+const controls = new PointerLockControls(camera, document.body);
+document.addEventListener('click', () => controls.lock());
+
+const activeProjectiles = [];
+window.addEventListener('mousedown', (e) => {
+    if (e.button === 0 && controls.isLocked) {
+        const projectile = {
+            position: camera.position.clone(),
+            velocity: new THREE.Vector3(0, 0, -1).applyQuaternion(camera.quaternion).multiplyScalar(715),
+            isDestroyed: false,
+            currentTemp: 15,
+            specs: {
+                physics: {mass: {boltCarrier: 0.008}},
+                barrel: {specs: {dragCofficient: 0.3}, thermals: {accuracyDegradation: 0.5}},
+                thermals: {accuracyDegradation: 0.05}
+            },
+            crossSectionArea: 0.000045
+        }
+    };
+    const geo = new THREE.SphereGeometry(0.05);
+    const mat = new THREE.MeshBasicMaterial({color: 0xfff000});
+    const mesh = new THREE.Mesh(geo, mat);
+    projectile.mesh = mesh;
+    scene.add(mesh);
+    
+})
